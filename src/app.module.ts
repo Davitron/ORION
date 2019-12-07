834b9f1c32a72a86a54ea206d49173083625b3ad
Davitron <baizoualexandre@yahoo.fr>
2019-12-08 00:17:29 +0100

    feat(tag):  setup env config and database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const config = new ConfigService('.env');
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule,
+    MongooseModule.forRoot(config.get('DATABASE_URL'), { useNewUrlParser: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
